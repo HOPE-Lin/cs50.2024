c4ae9e188c7799b854c0418f858e3641eb6223d7
HOPE-Lin <2686240473@qq.com>
2024-07-15 12:08:37 +0000

    Mon, Jul 15, 2024, 8:08 PM +08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,7 +5,7 @@ int main(void)
     int number,i,j;
     do{
         number = get_int("Height:\n");
-    }while(number<0);
+    }while(number<=0);
     for(i=1;i<=number;i++){
         for(j=1;j<=i;j++){
             printf("#");
