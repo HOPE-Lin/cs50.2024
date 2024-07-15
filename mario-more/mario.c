87015fc0e2da2bf4415de219f0a5760074f4c706
HOPE-Lin <2686240473@qq.com>
2024-07-15 12:52:14 +0000

    Mon, Jul 15, 2024, 8:52 PM +08:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -2,11 +2,26 @@
 #include <stdio.h>
 int main(void)
 {
-    int number;
+    int number, space, i, j;
     do
     {
         number = get_int("Height: ");
     }
     while(number<1||number>8);
+    for(i=0;i<number;i++)
+    {
+        space = number - 1 - i;
+        row(i+1);
+    }
+}
+void row(int space, int i)
+{
+    int k;
+    for(k = 0; k < space;k++)
+    {
+        printf(" ");
+    }
 
 }
+
+
