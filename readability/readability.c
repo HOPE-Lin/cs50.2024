1b73c985f81dcf02454d0390db5a94607b918695
HOPE-Lin <2686240473@qq.com>
2024-07-16 08:20:18 +0000

    Tue, Jul 16, 2024, 4:20 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,12 +3,20 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+
+int count_sentences(string text);
+
+int count_words(string text);
+
+int count_letters(string text);
+
 int main(void)
 {
     string text = get_string("Text: ");
     int letters = count_letters(text);
     int words = count_words(text);
     int sentences = count_sentences(text);
+    
 }
 int count_letters(string text)
 {
@@ -28,6 +36,18 @@ int count_words(string text)
     for(i = 0, len = strlen(text); i < len; i++)
     {
         if (isblank(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int count_sentences(string text)
+{
+    int i, count = 0;
+    for (i = 0, len = strlen(text); i < len; i++)
+    {
+        if (ispunct(text[i]))
         {
             count++;
         }
