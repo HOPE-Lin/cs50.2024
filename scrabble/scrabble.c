4c1e990962f147a08fbb78f39a05ecdb4609c05f
HOPE-Lin <2686240473@qq.com>
2024-07-16 04:13:42 +0000

    Tue, Jul 16, 2024, 12:13 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+
+int compute(string word);
+
 int main(void)
 {
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
-    
+    int score1 = compute(word1);
+    int score2 = compute(word2);
 }
+int compute(string word)
+{}
