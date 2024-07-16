d7d4b26921efc26aeb2f60c03e710545755c6218
HOPE-Lin <2686240473@qq.com>
2024-07-16 04:17:55 +0000

    Tue, Jul 16, 2024, 12:17 PM +08:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <ctype.h>
 
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
 int compute(string word);
 
 int main(void)
@@ -13,4 +15,10 @@ int main(void)
     int score2 = compute(word2);
 }
 int compute(string word)
-{}
+{
+    int score = 0;
+    for (int i = 0, len = strlen(word); i < len; i++)
+    {
+        
+    }
+}
