615ca510c25f6d0f27b5932c4ba489aad365296b
HOPE-Lin <2686240473@qq.com>
2024-07-17 07:36:39 +0000

    Wed, Jul 17, 2024, 3:36 PM +08:00

---
+++ substitution/substitution.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf(")
+    }
+}
+
