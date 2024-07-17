76eb0c082369bd5b945c4ae6df0478b4563c097a
HOPE-Lin <2686240473@qq.com>
2024-07-17 04:10:28 +0000

    Wed, Jul 17, 2024, 12:10 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,11 +2,32 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
+bool only_digits(string);
+
 int main(int argc, string argv[])
 {
-    if(argc != 2)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
+    bool answer = only_digits(argv[1]);
+    if (answer == 0){
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
+bool only_digits(string text)
+{
+    bool anwers = true;
+    for(int i = 0, len = strlen(text); i < len; i++)
+    {
+        if ( ! isdigit(string[i]))
+        {
+            anwers = false;
+            return anwers;
+        }
+    }
+    return anwers;
 }
