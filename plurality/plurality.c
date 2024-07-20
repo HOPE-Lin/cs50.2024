4d26a9768b2a8af114b7f8c679a357409da2acd1
HOPE-Lin <2686240473@qq.com>
2024-07-20 08:59:43 +0000

    Sat, Jul 20, 2024, 4:59 PM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -82,7 +82,7 @@ bool vote(string name)
 void print_winner(void)
 {
     // TODO
-    int tmp;
+    int tmp = -1;
     for (int i = 0; i < candidate_count; i++)
     {
         if(tmp < candidates[i].votes)
