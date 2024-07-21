b244adbfea472605821bfe5f92aa6272d881fb3e
HOPE-Lin <2686240473@qq.com>
2024-07-21 03:41:10 +0000

    Sun, Jul 21, 2024, 11:41 AM +08:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -143,6 +143,13 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
+    for (int i = 0; i < voter_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if(strcmp(name, candidates[i].name)
+        }
+    }
     return;
 }
 
