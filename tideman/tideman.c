5b55503aff7e0e31a17fbe6e4fce525156c1bd93
HOPE-Lin <2686240473@qq.com>
2024-07-21 12:22:29 +0000

    Sun, Jul 21, 2024, 8:22 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -158,7 +158,7 @@ void sort_pairs(void)
     {
         for(int j = i + 1; j < pair_count; i++)
         {
-            if (preferences[pairs[j].winner][pairs[j].loser] > preferences[pairs[i].winner][pairs[j].loser])
+            if (preferences[pairs[j].winner][pairs[j].loser] > preferences[pairs[i].winner][pairs[i].loser])
             {
                 tmp.winner = pairs[j].winner;
                 tmp.loser = pairs[j].loser;
