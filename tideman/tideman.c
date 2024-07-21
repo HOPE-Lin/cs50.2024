e9651f1e02795c19f0fc4a39cb860ccea3fcb1ca
HOPE-Lin <2686240473@qq.com>
2024-07-21 08:19:31 +0000

    Sun, Jul 21, 2024, 4:19 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,8 +117,8 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i; j < candidate_count; j++)
-        preferences[rank[i]][]
+        for (int j = i + 1; j < candidate_count; j++)
+        preferences[ranks[i]][ranks[j]] 
     }
     return;
 }
