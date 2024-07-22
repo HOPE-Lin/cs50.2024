a0e486f126a127b2f93cf9e5d6e6b7169edf32bf
HOPE-Lin <2686240473@qq.com>
2024-07-22 09:13:16 +0000

    Mon, Jul 22, 2024, 5:13 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -193,7 +193,7 @@ void lock_pairs(void)
             {
                 w = 0;
                 dfs(p, room, pairs[i].winner, pairs[i].loser);
-                break;
+                return;
             }
         }
     }
