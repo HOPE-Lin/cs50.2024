6654981e6c33a9d7e5805b8bd3980aad52f4b461
HOPE-Lin <2686240473@qq.com>
2024-07-25 09:02:07 +0000

    Thu, Jul 25, 2024, 5:02 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -48,7 +48,8 @@ unsigned int hash(const char *word)
     int j = 0;
     for (int i = 0; word[i] != '\0';i++)
     {
-        if(char[i] == )
+        if(word[i] == '\'')
+            continue;
         j += (tolower(word[i]) - 'a') * i * 37;
     }
     return j % 500;
