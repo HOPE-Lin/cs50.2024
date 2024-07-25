18e8e638d98f4ad8e3eb7f95f349ebc40ff8c2b3
HOPE-Lin <2686240473@qq.com>
2024-07-25 00:22:17 +0000

    Thu, Jul 25, 2024, 8:22 AM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -65,11 +65,12 @@ person *create_family(int generations)
         new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
-        new_person->a
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return new_person;
 }
 
 // Free `p` and all ancestors of `p`.
