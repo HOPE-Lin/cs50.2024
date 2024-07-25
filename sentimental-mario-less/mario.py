5e5c633026ee0548c85437df93686a3fe977350b
HOPE-Lin <2686240473@qq.com>
2024-07-25 14:19:59 +0000

    Thu, Jul 25, 2024, 10:19 PM +08:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,8 +1,8 @@
 from cs50 import get_int
-t = get_int("Height: ")
-while t > 8 or t <= 0:
+#t = get_int("Height: ")
+do:
     t = get_int("Height: ")
-
+while t > 8 or t <= 0
 for p in range(1, t + 1, 1):
     s = t - p
     for _ in range(s):
