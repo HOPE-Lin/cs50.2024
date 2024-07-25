78d42dc98cde00be202b62dc26e000000462c55e
HOPE-Lin <2686240473@qq.com>
2024-07-25 23:43:58 +0000

    Fri, Jul 26, 2024, 7:43 AM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,2 +1,16 @@
 from cs50 import get_float
-while 
+t = get_float
+p = 0
+while t >= 25:
+    t -= 25
+    p += 1
+while t >= 10:
+    t -= 10
+    p += 1
+while t >= 5:
+    t -= 5
+    p += 1
+while t >= 1:
+    t -= 1
+    p += 1
+    print(" ", p)
