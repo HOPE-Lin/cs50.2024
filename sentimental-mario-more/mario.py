ec5b38bdb4d172695de66fa134c966ec0df070b4
HOPE-Lin <2686240473@qq.com>
2024-07-25 23:26:38 +0000

    Fri, Jul 26, 2024, 7:26 AM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,3 +1,12 @@
 from cs50 import get_int
 t = get_int("Height :")
+while t <= 0 or t > 8:
+    t = get_int("Height:")
+for p in range(1, t + 1, 1):
+    s = t - p
+    for _ in range(s):
+        print(" ", end="")
+    for _ in range(t):
+        print("#", end="")
+    
 
