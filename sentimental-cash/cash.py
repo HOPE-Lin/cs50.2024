c7e15f66e39f3692d3b6bbb361c6524e43afaefe
HOPE-Lin <2686240473@qq.com>
2024-07-25 23:45:50 +0000

    Fri, Jul 26, 2024, 7:45 AM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,16 +1,16 @@
 from cs50 import get_float
-t = get_float
+t = get_float("Change: ")
 p = 0
-while t >= 25:
+while t >= 0.25:
     t -= 25
     p += 1
-while t >= 10:
+while t >= 0.10:
     t -= 10
     p += 1
-while t >= 5:
+while t >= 0.05:
     t -= 5
     p += 1
-while t >= 1:
+while t >= 0.01:
     t -= 1
     p += 1
-    print(" ", p)
+    print("", + p)
