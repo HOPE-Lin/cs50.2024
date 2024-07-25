6be48dbbb9bbafb972e4586d04c269f46aa8ed3d
HOPE-Lin <2686240473@qq.com>
2024-07-25 13:45:27 +0000

    Thu, Jul 25, 2024, 9:45 PM +08:00

--- mario.py/mario.py
+++ mario.py/mario.py
@@ -2,6 +2,9 @@ from cs50 import get_int
 t = get_int("Height: ")
 if t > 8 and t < 0:
     print("")
-for _ in range(t):
-    
+for p in range(t):
+    s = t - p
+    for _ in range(s):
+        print()
+
 
