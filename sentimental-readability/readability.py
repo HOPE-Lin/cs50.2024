dfd820fe119956562e1d021e4682b067cfb31db2
HOPE-Lin <2686240473@qq.com>
2024-07-26 02:12:43 +0000

    Fri, Jul 26, 2024, 10:12 AM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,5 +2,12 @@ from cs50 import get_string
 
 s = get_string("Text: ")
 t = len(s)
-for _ in range(t):
-    
+a = 0
+b = 0
+c = 0
+for i in range(t):
+    if s[i].isalpha:
+        a += 1
+    elif s[i].isspace():
+        b += 1
+    elif s[i] in [. ]
