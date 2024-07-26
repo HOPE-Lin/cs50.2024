47cc1418206b3f45e3abee8582f3a793f52bdc13
HOPE-Lin <2686240473@qq.com>
2024-07-26 04:12:35 +0000

    Fri, Jul 26, 2024, 12:12 PM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,18 +12,19 @@ def main():
     # TODO: Read database file into a variable
     rows = []
     i = 0
-    with open('sys.argv[1]') as file:
+    with open('sys.argv[1]', 'r') as file:
+        firster = file.readline()
         reader = csv.DictReader(file)
         for row in reader:
             rows.append(row)
             i += 1
 
     # TODO: Read DNA sequence file into a variable
-    with open('sys.argv[2]') as file:
+    with open('sys.argv[2]', 'r') as file:
         content = file.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    for 
+    for
 
     # TODO: Check database for matching profiles
 
