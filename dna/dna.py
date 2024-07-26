4cecc6c4fd4d75c4e13ad839899f2f222dd7147f
HOPE-Lin <2686240473@qq.com>
2024-07-26 09:57:18 +0000

    Fri, Jul 26, 2024, 5:57 PM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -36,7 +36,7 @@ def main():
     for p in range(i):
         match_found = True
         for j in range(k):
-            print("longests[j]: ", longests[j])
+            #print("longests[j]: ", longests[j])
             #print("rows[p].values(): ", list(rows[p].values())[j + 1])
             if longests[j] != int(list(rows[p].values())[j + 1]):
                  match_found = False
