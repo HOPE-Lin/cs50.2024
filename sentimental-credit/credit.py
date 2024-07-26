55da828145fd44c17580e346cf61698e2d59718a
HOPE-Lin <2686240473@qq.com>
2024-07-26 01:17:06 +0000

    Fri, Jul 26, 2024, 9:17 AM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,13 +1,25 @@
 import re
 
+def is_even(s):
+    
+
 def main():
     s = input("Number: ")
     t = s.len
     if t == 15:
         if re.match("^(34|37)", s):
-            if 
-    if t == 16:
+            if is_even(s):
+                print()
+    elif t == 16:
         if re.match("^(51|52|53|54|55)", s):
+            if is_even(s):
+                print()
         if re.match("^(4)", s):
-    if t == 13:
+            if is_even(s):
+                print()
+    elif t == 13:
         if re.match("^(4)", s):
+            if is_even(s):
+                print()
+    else:
+        print()
