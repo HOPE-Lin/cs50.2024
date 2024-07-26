49c0602b75548035f99a8345b4b330259e561e74
HOPE-Lin <2686240473@qq.com>
2024-07-26 02:20:17 +0000

    Fri, Jul 26, 2024, 10:20 AM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -12,4 +12,11 @@ for i in range(t):
         b += 1
     elif s[i] in ['.', '?',]:
        c += 1
-g = 0.0588 * a
+g = 0.0588 * (a / b) * 100 - 0.296 * (c / b) * 100 - 15.8
+if g > 16:
+    print("Grade 16+")
+elif g < 1:
+    print("Before Grade 1")
+else:
+    print("Grade " + g)
+
