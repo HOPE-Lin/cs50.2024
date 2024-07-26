3717ed2024451fd09c0a38d87ebef7a40a1760f6
HOPE-Lin <2686240473@qq.com>
2024-07-26 01:13:06 +0000

    Fri, Jul 26, 2024, 9:13 AM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,8 +4,10 @@ def main():
     s = input("Number: ")
     t = s.len
     if t == 15:
-        if re.match("^('34'|'37'), s):
+        if re.match("^(34|37)", s):
+            if 
     if t == 16:
-        if re.match("^('51'|'52'|'53'|'54'|'55'), s):
+        if re.match("^(51|52|53|54|55)", s):
+        if re.match("^(4)", s):
     if t == 13:
-        if re.match("^('34'|'37'), s):
+        if re.match("^(4)", s):
