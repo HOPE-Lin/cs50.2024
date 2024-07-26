13969a7879ac8919bf0a3290f99dff63cadbf3e7
HOPE-Lin <2686240473@qq.com>
2024-07-26 02:47:33 +0000

    Fri, Jul 26, 2024, 10:47 AM +08:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,7 +11,7 @@ for i in range(t):
     elif s[i].isspace():
         b += 1
     elif s[i] in ['.', '?',]:
-       c += 1
+        c += 1
 g = 0.0588 * (a / b) * 100 - 0.296 * (c / b) * 100 - 15.8
 if g > 16:
     print("Grade 16+")
@@ -19,4 +19,3 @@ elif g < 1:
     print("Before Grade 1")
 else:
     print(f"Grade {round(g)}")
-
