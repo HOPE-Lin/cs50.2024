307788a26764091f8723a177f2eafaaa1ca442ae
HOPE-Lin <2686240473@qq.com>
2024-07-27 12:59:18 +0000

    Sat, Jul 27, 2024, 8:59 PM +08:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,19 +1,19 @@
 SELECT title
 FROM movies
+    /*CASE
+        WHEN rating = (SELECT MAX(rating) FROM ratings) THEN 1
+        ELSE 0
+    END AS ma_rat*/
 WHERE id IN
 (
     SELECT stars.movie_id
     FROM stars
     JOIN ratings ON stars.movie_id = ratings.movie_id
-    CASE
-        WHEN rating = (SELECT MAX(rating) FROM ratings) THEN 1
-        ELSE 0
-    END AS ma_rat
     WHERE person_id = (
         SELECT id
         FROM people
         WHERE name = 'Chadwick Boseman'
     )
-    ORDER BY rating DESC
-    LIMIT 5
-);
+)
+ORDER BY rating DESC
+LIMIT 5;
