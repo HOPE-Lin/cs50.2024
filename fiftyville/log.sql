0d1a422c4aa9dd9f14226b71f7a45fce3e2001ff
HOPE-Lin <2686240473@qq.com>
2024-07-27 23:44:01 +0000

    Sun, Jul 28, 2024, 7:44 AM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,9 +1,10 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
---找到犯罪
-SELECT id，description
+--找到犯罪现场描述
+SELECT description
 FROM crime_scene_reports
 WHERE year = 2023
   AND month = 7
   AND day = 28
   AND street = 'Humphrey Street'
   AND  description LIKE '%CS50%';
+  --了解到发生时间：10.15am，
