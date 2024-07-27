5982ff78437892ee00b5c076090397fc7b7777a6
HOPE-Lin <2686240473@qq.com>
2024-07-27 09:44:12 +0000

    Sat, Jul 27, 2024, 5:44 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,2 +1,18 @@
-SELECT name
+SELECT DISTINCT name
 FROM people
+WHERE id IN
+(
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN
+    (
+        SELECT movie_id
+        FROM stars
+        WHERE person_id =(
+            SELECT id
+            FROM people
+            WHERE name ='Kevin Bacon'
+        )
+    )
+)
+NOT IN ('Kevin Bacon');
