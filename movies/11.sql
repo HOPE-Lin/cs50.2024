84500d4411df4f1a73c3785ffe91f6c2fba4fc38
HOPE-Lin <2686240473@qq.com>
2024-07-27 09:23:36 +0000

    Sat, Jul 27, 2024, 5:23 PM +08:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,5 +2,13 @@ SELECT title
 FROM movies
 WHERE id IN
 (
-    
+    SELECT movie_id
+    FROM directors
+    JOIN ratings ON people.id = ratings.movie_id
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = 'Chadwick Boseman'
+    )
+    ORDER BY 
 )
