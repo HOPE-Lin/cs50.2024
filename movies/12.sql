2bbb6020b283b4791ae194df52e152d502fec9ff
HOPE-Lin <2686240473@qq.com>
2024-07-27 13:15:25 +0000

    Sat, Jul 27, 2024, 9:15 PM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -8,7 +8,7 @@ WHERE id IN
     (
         SELECT id
         FROM people
-        WHERE name ='Bradley Cooper' OR name = 'Jennifer Lawrence starred'
+        WHERE name ='Bradley Cooper' OR name = 'Jennifer Lawrence'
     )
     GROUP BY movie_id
     HAVING COUNT(movie_id) = 2
