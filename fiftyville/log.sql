75d0cb5766cbd472071c24399535e82c2fffb0ab
HOPE-Lin <2686240473@qq.com>
2024-07-27 23:28:05 +0000

    Sun, Jul 28, 2024, 7:28 AM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT id, description
 FROM crime_scene_reports
-WHERE 
+WHERE year = 2023
+  AND month = 'July'
+  AND day = 28
+  AND street = 'Humphrey Street';
