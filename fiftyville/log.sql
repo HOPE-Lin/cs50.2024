ea5c18a38a8b2adc71d106707ad63e4fad8d0a51
HOPE-Lin <2686240473@qq.com>
2024-07-28 01:16:56 +0000

    Sun, Jul 28, 2024, 9:16 AM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -61,4 +61,5 @@ WHERE year = 2023
     AND month = 7
     AND day = 28
     AND duration < 60
-JOIN people on  people.phone_number = 
+JOIN people on  people.phone_number = phone_calls.caller
+JOIN people on  people.phone_number = phone_calls.receiver
