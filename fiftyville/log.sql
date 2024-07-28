0b092e5f5f864bcff468806618c93eba567df9c6
HOPE-Lin <2686240473@qq.com>
2024-07-28 00:11:42 +0000

    Sun, Jul 28, 2024, 8:11 AM +08:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,13 +8,16 @@ WHERE year = 2023
   AND street = 'Humphrey Street'
   AND  description LIKE '%CS50%';
   --了解到发生时间：10.15am，三个目击者，详细地点是bakery
-SELECT transcript，name
+  --了解目击者的叙述
+SELECT transcript, name
 FROM interviews
 WHERE year = 2023
   AND month = 7
   AND day = 28
   AND transcript LIKE '%bakery%';
-  --10.15am ——10.25am小偷开汽车逃走，
+  --10.15am ——10.25am小偷开汽车逃走；
+  --在Eugene到达面包店之前，小偷在Leggett Street ATM 上取钱；
+  --打电话让同伙买29日最早的航班离开Fiftyville，电话持续不到一分钟
 /*SELECT activity, license_plate
 FROM bakery_security_logs
 WHERE year = 2023
